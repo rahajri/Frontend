3410588d4a17524291bc66821a336c84a65886f6
Abdelkarim AIT HQI <99421263+ait-79i@users.noreply.github.com>
2025-04-10 21:51:00 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
   backgroundColor: '#FFF8F5',
   warningyColor: '#05445e',
   defaultLanguage: 'fr',
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'https://agile-attitude.com/api',
 };
 
 /*
