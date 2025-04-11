96f5bdee52be29f71b4d8b1d9ee4ae59f075b47b
rahajri <34888480+rahajri@users.noreply.github.com>
2025-04-12 01:45:02 +0200

    Update ocr.service.prod.ts

--- src/app/core/services/ocr.service.prod.ts
+++ src/app/core/services/ocr.service.prod.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class OcrService {
-  private apiUrl = 'https://ocr.interim-online.fr/ocr/upload';
+  private apiUrl = 'https://wwww.interim-online.fr/ocr/upload';
 
   constructor(private http: HttpClient) {}
 
