1963b5a7b6f09da60b396530b8fa9f43f30cdaf8
AIT HQI Abdelkarim <abdelkarimaithqi@gmail.com>
2025-04-29 23:31:28 +0000

    add CV column to candidats list in admin side & modify SEO conf

--- src/app/features-modules/admin/freelancers/freelancers.module.ts
+++ src/app/features-modules/admin/freelancers/freelancers.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FreelancersRoutingModule } from './freelancers-routing.module';
 import { FreelancersComponent } from './freelancers.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 @NgModule({
   declarations: [FreelancersComponent],
-  imports: [CommonModule, FreelancersRoutingModule, SharedModule],
+  imports: [
+    CommonModule,
+    FreelancersRoutingModule,
+    SharedModule,
+    PdfViewerModule,
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class FreelancersModule {}
